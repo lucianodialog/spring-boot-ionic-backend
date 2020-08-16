8a0d650ea2090345f45951ec1e29d61b3da107a4
Luciano Silva <luciano_dialog@yahoo.com.br>
2020-08-16 20:18:41 +0000

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -39,11 +39,11 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	/*
+	
 	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
-	*/
+	
 	public Cliente() {
 	}
 
@@ -111,7 +111,7 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
-/*
+
 	public List<Pedido> getPedidos() {
 		return pedidos;
 	}
@@ -119,7 +119,7 @@ public class Cliente implements Serializable {
 	public void setPedidos(List<Pedido> pedidos) {
 		this.pedidos = pedidos;
 	}
-*/
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
