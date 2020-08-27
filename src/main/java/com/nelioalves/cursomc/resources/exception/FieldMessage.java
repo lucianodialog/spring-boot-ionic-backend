20ec64849f72abc9b29c37fe1b41d830aea97448
Luciano <luciano_dialog@yahoo.com.br>
2020-08-27 18:03:11 +0000

    Personalizando o retorno da validação

---
+++ src/main/java/com/nelioalves/cursomc/resources/exception/FieldMessage.java
@@ -0,0 +1,39 @@
+package com.nelioalves.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String FieldName;
+	private String message;
+	
+	public FieldMessage(String fieldName, String message) {
+		super();
+		FieldName = fieldName;
+		this.message = message;
+	}
+
+	public String getFieldName() {
+		return FieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		FieldName = fieldName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+	
+	
+	
+
+}
--- src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,17 +17,27 @@ import com.nelioalves.cursomc.services.exception.ObjectNotFoundException;
 public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandarError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){		
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){		
 		
-		StandarError err = new StandarError(HttpStatus.NOT_FOUND.value(), e.getMessage() ,System.currentTimeMillis());
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage() ,System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 	
 	@ExceptionHandler(DataIntegrityException.class)
-	public ResponseEntity<StandarError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 		
-		StandarError err = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de Validação!", System.currentTimeMillis());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addErrors(x.getField(), x.getDefaultMessage());
+		}		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(err);
 	}
 
 }
